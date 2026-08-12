95d705c852567e262e0c2cd211794b2d5a48e809
LENOVO <LENOVO@LENOVO>
2026-08-12 23:48:56 +0530

    created user Entity

--- Entities/User.cs
+++ Entities/User.cs
@@ -1,6 +1,24 @@
-﻿namespace EduPulse_API.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduPulse_API.Entities
 {
     public class User
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FullName { get; set; } = string.Empty;
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+        [Required]
+        public string Role { get; set; } = string.Empty; // Admin, Teacher, Student
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime? UpdatedAt { get; set; }
     }
 }
