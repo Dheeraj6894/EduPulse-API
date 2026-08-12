08736a97ef7d40f5e103a26b75f1125c718fdb51
LENOVO <LENOVO@LENOVO>
2026-08-12 23:52:52 +0530

    fix

--- Entities/User.cs
+++ Entities/User.cs
@@ -9,6 +9,7 @@ namespace EduPulse_API.Entities
         [Required]
         public string FullName { get; set; } = string.Empty;
         [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
         [Required]
         public string PasswordHash { get; set; } = string.Empty;
