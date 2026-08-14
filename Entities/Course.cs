7251ad1576e72cfe53f256a3068819fe308a8898
LENOVO <LENOVO@LENOVO>
2026-08-15 02:51:10 +0530

    course and subject entity added

---
+++ Entities/Course.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduPulse_API.Entities
+{
+    public class Course
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string CourseCode { get; set; } = string.Empty;
+        [Required]
+        public string CourseName { get; set; } = string.Empty;
+        [Required]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        [Range(1, 6)]
+        public int DurationInYears { get; set; }
+    }
+}
